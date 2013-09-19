bbe869823a993bfdd6b5dba6e5e3d74bf7ee3d69
c25050 <c25050@oakleaf-fx-1.(none)>
2013-09-19 22:08:55 +0900

    scripts for FX10

---
+++ flatMPI/flatMPI.FX10-n0064.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#PJM -L "rscgrp=short"
+#PJM -L "elapse=1:00:00"
+#PJM -L    "node=64"
+#PJM --mpi "proc=1024"
+#PJM -o flatMPI.FX10-n0064.out
+#PJM -e flatMPI.FX10-n0064.err
+export FLIB_FASTOMP=TRUE
+export OMP_NUM_THREADS=1
+export PARALLEL=1
+export FLIB_CNTL_BARRIER_ERR=FALSE
+mpiexec ./fft_check_mpi  20 1024 1024 1024 in
+mpiexec ./fft_check_mpi  40 1024 1024 1024 in
+mpiexec ./fft_check_mpi  20 1024 1024 1024 out
+mpiexec ./fft_check_mpi  40 1024 1024 1024 out
+
+mpiexec ./fft_check_mpi  10 2048 2048 2048 in
+mpiexec ./fft_check_mpi  20 2048 2048 2048 in
+mpiexec ./fft_check_mpi  10 2048 2048 2048 out
+mpiexec ./fft_check_mpi  20 2048 2048 2048 out
+
+mpiexec ./fft_check_mpi  10 3072 3072 3072 in
+mpiexec ./fft_check_mpi  20 3072 3072 3072 in
+mpiexec ./fft_check_mpi  10 3072 3072 3072 out
+mpiexec ./fft_check_mpi  20 3072 3072 3072 out
+
+mpiexec ./fft_check_mpi  10 4096 4096 4096 in
+mpiexec ./fft_check_mpi  20 4096 4096 4096 in
