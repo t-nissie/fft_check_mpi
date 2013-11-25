2398f075dfe95239036c5bf40908fa27bf3c54a3
Takeshi Nishimatsu <t-nissie@imr.tohoku.ac.jp>
2013-11-25 17:26:13 +0900

    Now, we can generate index.html form README.md locally.

---
+++ index_html_generator.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+##
+cat > index.html <<- EOF
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	  <title>fft_check_mpi.F</title>
+	  <meta name="author" content="Takeshi Nishimatsu" />
+	  <meta name="copyright" content="Copyright &#169; 2013 Takeshi Nishimatsu" />
+	  <link rel="stylesheet" href="style.css" type="text/css" />
+	  <link rel="shortcut icon" href="favicon.ico" />
+	</head>
+	<body>
+EOF
+
+~/gems/github-markdown-0.6.3/bin/gfm --readme README.md >> index.html
+
+cat >> index.html <<- EOF
+	</body>
+	</html>
+EOF
